b92ed71f6feaaf3fb427ea8098fae536624afece
Abdwahab <adekolaabdwahababisoye@gmail.com>
2021-10-06 13:20:29 -0700

    get course number function

--- dataNormalisation.js
+++ dataNormalisation.js
@@ -1,9 +1,47 @@
+
+
 let normalizeCode=(code)=>{
 	let codeObject ={}
 
 	let ncDept = getDept(code)
 	codeObject.Department = ncDept
+
+	let ncCourseNumber = getCourseNumber(code, ncDept)
+	codeObject.courseNumber = ncCourseNumber.courseNumber
+
+	let {yearAndSemCode}  = ncCourseNumber
+
+
 	return codeObject
+
+}
+
+let getYearAndSemester =()=>{
+
+}
+
+
+
+
+let getCourseNumber =(code, ncDept)=>{
+	 let lengthOfCourseCode = ncDept.length
+	 let leftCode = code.slice(lengthOfCourseCode)
+
+
+	 if(isNaN(parseFloat(leftCode[0]))){
+	 	//then it is delimiter, cut it
+	 	leftCode = leftCode.slice(1)
+	 	let indexOfSpace = leftCode.indexOf(' ')
+	 	let courseNumber  = leftCode.slice(0,indexOfSpace)
+	 	let yearAndSemCode = leftCode.slice(indexOfSpace)
+	 	return {courseNumber, yearAndSemCode}
+	 }else{
+	 	//do not cut it
+	 	let indexOfSpace = leftCode.indexOf(' ')
+	 	let courseNumber = leftCode.slice(0,indexOfSpace)
+	 	let yearAndSemCode = leftCode.slice(indexOfSpace)
+	 	return {courseNumber, yearAndSemCode}
+	 }
 
 }
 
