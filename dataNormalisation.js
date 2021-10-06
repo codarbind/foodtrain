4d91a06a72e2d9b16359edd7f980c3d1dfa0e38d
Abdwahab <adekolaabdwahababisoye@gmail.com>
2021-10-06 14:57:07 -0700

    getyearsem function and its helpers

--- dataNormalisation.js
+++ dataNormalisation.js
@@ -3,7 +3,7 @@
 let normalizeCode=(code)=>{
 	let codeObject ={}
 
-	let ncDept = getDept(code)
+	let ncDept = getCharBlock(code)
 	codeObject.Department = ncDept
 
 	let ncCourseNumber = getCourseNumber(code, ncDept)
@@ -11,12 +11,29 @@ let normalizeCode=(code)=>{
 
 	let {yearAndSemCode}  = ncCourseNumber
 
+	let ncYearAndSem = getYearAndSemester(yearAndSemCode)
+	codeObject.year = ncYearAndSem.year
+	codeObject.semester = ncYearAndSem.semester
 
 	return codeObject
 
 }
 
-let getYearAndSemester =()=>{
+let getYearAndSemester =(yearAndSemCode)=>{
+	yearAndSemCode = yearAndSemCode.trim()
+	if(isNaN(parseFloat(yearAndSemCode[0]))){
+		//then it is a character, indicating semester
+		let semester = getCharBlock(yearAndSemCode)
+		let year = yearAndSemCode.slice(semester.length).trim()
+		
+		return {semester,year}
+	}else{
+		//it is a number
+		let semester = getCharBlockReverse(yearAndSemCode)
+		let year = yearAndSemCode.slice(0, yearAndSemCode.length - semester.length).trim()
+		return {semester,year}
+
+	}
 
 }
 
@@ -45,15 +62,30 @@ let getCourseNumber =(code, ncDept)=>{
 
 }
 
-let getDept = (code)=>{
-		let dept = ''
+let getCharBlock = (code)=>{
+		let output = ''
 		for (let i = 0; i <= code.length - 1 ; i++) {
 		let char = code[i]
 		if(char.toLowerCase() == char.toUpperCase()){
 			i = code.length + 1 //force the loop to stop,then call next function
 		}else{
-			dept += char			
+			output += char			
 		}
 	}
-	return dept
+	return output
+}
+
+
+let getCharBlockReverse = (code)=>{
+		let output = ''
+		for (let i = code.length - 1; i >= 0 ; i--) {
+		let char = code[i]
+		if(char.toLowerCase() == char.toUpperCase()){
+			i = -1 //force the loop to stop,then call next function
+		}else{
+			output += char			
+		}
+	}
+	output = output.split('').reverse().join('')
+	return output
 }
