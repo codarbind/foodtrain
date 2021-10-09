151ea2ffcfbee64b0e6709a940680b492efc7d35
Abdwahab <adekolaabdwahababisoye@gmail.com>
2021-10-09 07:14:29 -0700

    home route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const normalisecode = require('./routes/normalisecode')
 
 app.use(normalisecode)
 
+app.get('/',(req,res)=>{
+	res.send("welcome to Food Train API endpoint, to normalise a course code, make GET request to '/normalisecode' with code as query parameter, and the course code as value")
+})
+
 
 const PORT = process.env.PORT || 4500
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
