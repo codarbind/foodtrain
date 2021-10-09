51924bcbad5fdb9958075b337a711d6bbd02aa8f
Abdwahab <adekolaabdwahababisoye@gmail.com>
2021-10-09 07:06:19 -0700

    handled input error

--- routes/normalisecode.js
+++ routes/normalisecode.js
@@ -5,7 +5,9 @@ const neededFunctions = require('../helperfunctions')
 router.get('/normalisecode',async (req,res)=>{
 
 	let {code} = req.query
-	let responseObject = await neededFunctions.normalizeCode(code)
+	console.log(code)
+	if(!code) return res.json({'errorMessage':"kindly call with course code: \'code\' as query parameter"})
+	let responseObject = (code.length >=6 && code.indexOf(' ') !== -1)? await neededFunctions.normalizeCode(code): {'errorMessage':'that is not a valid course code'}
 	
 	res.json(responseObject)
 
